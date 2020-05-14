e2cabc50692aeb6607b75277cd45a4a0b7a4c7f1
joao <joao.narciso@acertsis.com.br>
2020-05-13 21:15:28 -0300

    just sugar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ export const Header = () => {
     return (
         <>
             <header className="flex align-items-center justify-content-center">  {/* bootstrap classes  */}
-                <h1>MARVEL<span style={{ marginLeft: '-4px' }}>CARD</span></h1>  {/*correcting font by approaching the word "card" to "marvel" by 4px. in <h1> style.css this value is discounted from the background with padding-right = (padding-left -4px).*/}
+                <h1 onClick={() => window.location.reload()}>MARVEL<span style={{ marginLeft: '-4px' }}>CARD</span></h1>  {/*correcting font by approaching the word "card" to "marvel" by 4px. in <h1> style.css this value is discounted from the background with padding-right = (padding-left -4px).*/}
             </header>
         </>
     )
