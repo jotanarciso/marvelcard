7cb8c0bd0178fe8cb65f44e4246707c73c27bd7e
joao <joao.narciso@acertsis.com.br>
2020-05-13 21:52:26 -0300

    Updates

---
+++ precache-manifest.e3380b5f8dca8b9558e6f3cf7ce7b061.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8356385b6321618e78cba23996f302c8",
+    "url": "/marvelcards/index.html"
+  },
+  {
+    "revision": "8674be9f0c80a6eb5766",
+    "url": "/marvelcards/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "521f3bb7ab21c6826515",
+    "url": "/marvelcards/static/css/main.d72e6681.chunk.css"
+  },
+  {
+    "revision": "8674be9f0c80a6eb5766",
+    "url": "/marvelcards/static/js/2.138b7ea0.chunk.js"
+  },
+  {
+    "revision": "801455085fc6f57b23eb009849543052",
+    "url": "/marvelcards/static/js/2.138b7ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521f3bb7ab21c6826515",
+    "url": "/marvelcards/static/js/main.1524b1f5.chunk.js"
+  },
+  {
+    "revision": "e432893aa16956fe1ad7",
+    "url": "/marvelcards/static/js/runtime-main.7df991af.js"
+  },
+  {
+    "revision": "710391ab24f74cce9693d7b6df780cb8",
+    "url": "/marvelcards/static/media/foward.710391ab.png"
+  },
+  {
+    "revision": "067285b0c7e98187bfeb0fe492712d2e",
+    "url": "/marvelcards/static/media/marvel-font.067285b0.ttf"
+  }
+]);
